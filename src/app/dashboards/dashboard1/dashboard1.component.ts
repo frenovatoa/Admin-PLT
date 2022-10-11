5fca9498bd8e82448f99849afcd8dc9a8eed7db0
frenovatoa <frenovato@outlook.com>
2022-10-11 12:34:25 -0500

    Inserción de prueba comentada.

--- src/app/dashboards/dashboard1/dashboard1.component.ts
+++ src/app/dashboards/dashboard1/dashboard1.component.ts
@@ -18,9 +18,9 @@ export class Dashboard1Component {
             this.data=data
         });
         // Insertar un documento en la colección indicada en la función addData(data)
-        this.fb.addData({descripcion: 'Auxiliar', estatus: '1'}).then((data)=>{
-            console.log(data)
-        });
+        // this.fb.addData({descripcion: 'Auxiliar', estatus: '1'}).then((data)=>{
+        //     console.log(data)
+        // });
         // Actualizar un documento en específico de la colección indicada en la función updateData(id, data)
         this.fb.updateData('z50EJXiQIk3yNKVcITH0', {descripcion: 'Auxiliar', estatus: '1'});
         this.fb.updateData('vzoyLcJvQb4Kv66Trcoa', {descripcion: 'Administrador', estatus: '1'});
