b4c3373dab5b07ec23f550ddd9aa93aafcde8084
frenovatoa <frenovato@outlook.com>
2022-10-12 12:26:13 -0500

    Creación del componente y diálogo de productos.

---
+++ src/app/pages/catalogs/products/products-dialog/products-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products-dialog',
+  templateUrl: './products-dialog.component.html',
+  styleUrls: ['./products-dialog.component.scss']
+})
+export class ProductsDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
