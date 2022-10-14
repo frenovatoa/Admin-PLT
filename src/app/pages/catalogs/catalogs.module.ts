bafc8ffe61ae9571928f9a6539da3faf4f8e10aa
Jose Bautista <bautistalarajosenatividad@gmail.com>
2022-10-14 16:49:00 -0500

    importacion de modulos en el modulo de catalogos

--- src/app/pages/catalogs/catalogs.module.ts
+++ src/app/pages/catalogs/catalogs.module.ts
@@ -5,8 +5,14 @@ import { ProductsComponent } from './products/products.component';
 import { TypeOfProductsComponent } from './type-of-products/type-of-products.component';
 import { CatalogsRoutingModule } from './catalogs-routing.module';
 import { TypeOfProductsDialogComponent } from './type-of-products/type-of-products-dialog/type-of-products-dialog.component';
-
-
+import {MatDialogModule} from '@angular/material/dialog';
+import { DemoMaterialModule } from 'src/app/demo-material-module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatListModule } from '@angular/material/list';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -17,7 +23,16 @@ import { TypeOfProductsDialogComponent } from './type-of-products/type-of-produc
   ],
   imports: [
     CommonModule,
-    CatalogsRoutingModule
+    CatalogsRoutingModule,
+    MatDialogModule,
+    DemoMaterialModule,
+    FlexLayoutModule,
+    FormsModule,
+    MatListModule,
+    ReactiveFormsModule,
+    NgxDatatableModule,
+    MatExpansionModule,
+    SharedModule
   ]
 })
 export class CatalogsModule { }
