f55510673fd40c2a7be96cbf4beb56c01b65e6cd
frenovatoa <frenovato@outlook.com>
2022-10-24 11:32:24 -0500

    Inserción y muestra de productos.

--- src/app/pages/catalogs/products/products-dialog/products-dialog.component.ts
+++ src/app/pages/catalogs/products/products-dialog/products-dialog.component.ts
@@ -1,4 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, Inject, OnInit, Optional, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/shared/auth/auth.service';
+import { Product } from 'src/app/shared/interfaces/product';
+import { ProductType } from 'src/app/shared/interfaces/product.type';
+import { ProductService } from 'src/app/shared/services/product.service';
+import { ProductTypeService } from 'src/app/shared/services/product.type.service';
 
 @Component({
   selector: 'app-products-dialog',
@@ -7,9 +19,140 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsDialogComponent implements OnInit {
 
-  constructor() { }
+  public dataSource: MatTableDataSource<ProductType>;    
+  searchText: any;
+
+  private started: boolean = false;
+
+  // Inicializar un arreglo vacío de tipos de productos
+  public productType: ProductType[]=[];
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  action: string;
+  // tslint:disable-next-line - Disables all
+  local_data: any;
+  selectedImage: any = '';
+
+  public formProducts: FormGroup;
+
+  constructor(
+    public datePipe: DatePipe,
+    public dialogRef: MatDialogRef<ProductsDialogComponent>,
+    public fb: FormBuilder,
+    public productService: ProductService,
+    public authService : AuthService,
+    public toastr :ToastrService,
+    // @Optional() is used to prevent error if no data is passed
+    @Optional() @Inject(MAT_DIALOG_DATA) public data: Product) {
+    this.local_data = { ...data };
+    this.action = this.local_data.action;
+    if (this.local_data.imagePath === undefined) {
+        this.local_data.imagePath = 'assets/images/products/default.png';
+    }
+    this.formProducts = this.fb.group({
+      id: [''],
+      productTypeId: [''],
+      description: ['', Validators.required],
+      quantity: ['', Validators.required],
+      image: [''],
+      status: ['', Validators.required],
+
+      /*userId: this.userData.id,*/ // Se debe de obtener al hacer patchValue()
+    });
+  }
 
   ngOnInit(): void {
+    this.productService.getProductTypes().subscribe((productType: any)=>{
+      console.log(productType)
+      this.productType=productType
+  });     
+  this.toastr.success("Producto Creado");
   }
+
+  // load(){
+  //   this.getProductTypes();
+  // }
+
+  /** Guarda registro */
+  save(): void {
+    let data = this.formProducts.value;
+    data.id = this.productService.unicID();
+    console.log(data)
+    if (this.formProducts.valid) {
+      // Inserción en la base de datos
+      this.productService.addProduct(data).then((product: any) => {
+        console.log(product)
+        this.toastr.success("Producto Creado.");
+      });
+      this.closeDialog();
+    } else {
+      this.toastr.error("Favor de llenar campos faltantes.");
+    }
+  }
+
+  /** Cerrar diálogo/modal */
+  // closeDialog(response?): void {
+  //   if (response != undefined) {
+  //     if (response["success"]) {
+  //       this.toastr.success(response["message"]);
+  //     } else {
+  //       this.toastr.error(response["message"]);
+  //     }
+  //     this.dialogRef.close(true);
+  //   } else {
+  //     this.dialogRef.close(false);
+  //   }
+  // }
+
+  // addData(type: number) {
+  //   switch (type) {
+  //       case 3:
+  //         /** Abre el dialogo de tipos de producto para añadir un nuevo registro */
+  //         const dialogType = this.dialog.open(ProductTypeDialogComponent, {
+  //           data: {
+  //             type: 1,
+  //             action: 'Nuevo'
+  //           }
+  //         }).afterClosed().subscribe((confirm: boolean) => {
+  //           if (confirm) {
+  //             this.getProductTypes();
+  //           } else {
+  //             this.toastr.error('Cancelado');
+  //           }
+  //         });
+  //     default:
+  //       break;
+  //   }
+  // }
+
+  doAction(): void {
+    this.dialogRef.close({ event: this.action, data: this.local_data });
+  }
+  closeDialog(): void {
+      this.dialogRef.close({ event: 'Cancelar.' });
+      this.toastr.success("Producto Creado.");
+  }
+
+  selectFile(event: any): void {
+    if (!event.target.files[0] || event.target.files[0].length === 0) {
+        // this.msg = 'You must select an image';
+        return;
+    }
+    const mimeType = event.target.files[0].type;
+    if (mimeType.match(/image\/*/) == null) {
+        // this.msg = "Only images are supported";
+        return;
+    }
+    // tslint:disable-next-line - Disables all
+    const reader = new FileReader();
+    reader.readAsDataURL(event.target.files[0]);
+    // tslint:disable-next-line - Disables all
+    reader.onload = (_event) => {
+        // tslint:disable-next-line - Disables all
+        this.local_data.imagePath = reader.result;
+    };
+}
 
 }
--- src/app/pages/catalogs/products/products.component.ts
+++ src/app/pages/catalogs/products/products.component.ts
@@ -1,125 +1,22 @@
+import { DatePipe } from '@angular/common';
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
-import { Product } from 'src/app/shared/interfaces/product';
+// import { Product } from 'src/app/shared/interfaces/product';
+import { ProductService } from 'src/app/shared/services/product.service';
 import { ProductsDialogComponent } from './products-dialog/products-dialog.component';
 
-// export interface Employee {
-//   id: number;
-//   Name: string;
-//   Position: string;
-//   Email: string;
-//   Mobile: number;
-//   DateOfJoining: Date;
-//   Salary: number;
-//   Projects: number;
-//   imagePath: string;
-// }
-
-// const employees = [
-//   {
-//       id: 1,
-//       Name: 'Johnathan Deo',
-//       Position: 'Seo Expert',
-//       Email: 'r@gmail.com',
-//       Mobile: 9786838,
-//       DateOfJoining: new Date('01-2-2020'),
-//       Salary: 12000,
-//       Projects: 10,
-//       imagePath: 'assets/images/users/2.jpg'
-//   },
-//   {
-//       id: 2,
-//       Name: 'Mark Zukerburg',
-//       Position: 'Web Developer',
-//       Email: 'mark@gmail.com',
-//       Mobile: 8786838,
-//       DateOfJoining: new Date('04-2-2020'),
-//       Salary: 12000,
-//       Projects: 10,
-//       imagePath: 'assets/images/users/3.jpg'
-//   },
-//   {
-//       id: 3,
-//       Name: 'Sam smith',
-//       Position: 'Web Designer',
-//       Email: 'sam@gmail.com',
-//       Mobile: 7788838,
-//       DateOfJoining: new Date('02-2-2020'),
-//       Salary: 12000,
-//       Projects: 10,
-//       imagePath: 'assets/images/users/4.jpg'
-//   },
-//   {
-//       id: 4,
-//       Name: 'John Deo',
-//       Position: 'Tester',
-//       Email: 'john@gmail.com',
-//       Mobile: 8786838,
-//       DateOfJoining: new Date('03-2-2020'),
-//       Salary: 12000,
-//       Projects: 11,
-//       imagePath: 'assets/images/users/5.jpg'
-//   },
-//   {
-//       id: 5,
-//       Name: 'Genilia',
-//       Position: 'Actor',
-//       Email: 'genilia@gmail.com',
-//       Mobile: 8786838,
-//       DateOfJoining: new Date('05-2-2020'),
-//       Salary: 12000,
-//       Projects: 19,
-//       imagePath: 'assets/images/users/6.jpg'
-//   },
-//   {
-//       id: 6,
-//       Name: 'Jack Sparrow',
-//       Position: 'Content Writer',
-//       Email: 'jac@gmail.com',
-//       Mobile: 8786838,
-//       DateOfJoining: new Date('05-21-2020'),
-//       Salary: 12000,
-//       Projects: 5,
-//       imagePath: 'assets/images/users/7.jpg'
-//   },
-//   {
-//       id: 7,
-//       Name: 'Tom Cruise',
-//       Position: 'Actor',
-//       Email: 'tom@gmail.com',
-//       Mobile: 8786838,
-//       DateOfJoining: new Date('02-15-2019'),
-//       Salary: 12000,
-//       Projects: 9,
-//       imagePath: 'assets/images/users/3.jpg'
-//   },
-//   {
-//       id: 8,
-//       Name: 'Hary Porter',
-//       Position: 'Actor',
-//       Email: 'hary@gmail.com',
-//       Mobile: 8786838,
-//       DateOfJoining: new Date('07-3-2019'),
-//       Salary: 12000,
-//       Projects: 7,
-//       imagePath: 'assets/images/users/6.jpg'
-//   },
-//   {
-//       id: 9,
-//       Name: 'Kristen Ronaldo',
-//       Position: 'Player',
-//       Email: 'kristen@gmail.com',
-//       Mobile: 8786838,
-//       DateOfJoining: new Date('01-15-2019'),
-//       Salary: 12000,
-//       Projects: 1,
-//       imagePath: 'assets/images/users/5.jpg'
-//   }
-// ];
+export interface Product {
+  id: string;
+  productTypeId: string;
+  description: string;
+  quantity: number;
+  image: string;
+  status: number;
+}
 
 @Component({
   selector: 'app-products',
@@ -129,27 +26,53 @@ import { ProductsDialogComponent } from './products-dialog/products-dialog.compo
 export class ProductsComponent implements OnInit, AfterViewInit {
 
   public products: Product[];
-  public firstFormGroup: FormGroup = Object.create(null);
+  // public firstFormGroup: FormGroup = Object.create(null);
   public dataSource: MatTableDataSource<Product>;
-  public displayedColumns = ['position', 'productTypeId', 'description', 'quantity', 'image', 'status', 'action'];
+  public displayedColumns: string[] = ['#', 'description', 'productType', 'quantity', 'status', 'action'];
   // public userData: User;
 
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
-  constructor(
-    // private toastr: ToastrService,
-    public dialog: MatDialog,
-    // private InsurerService: InsurerService,
-    private _formBuilder: FormBuilder
 
-  ) {
+  constructor(public dialog: MatDialog, public datePipe: DatePipe, public fb: ProductService) { 
 
   }
+
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+      // Obtener los documentos de la colección indicada en la función getUser()
+      this.fb.getProducts().subscribe((user: any)=>{
+        console.log(user)
+        this.products=user
+        this.dataSource = new MatTableDataSource < Product > (this.products);
+        console.log(this.dataSource);
+        this.dataSource.paginator =this.paginator;
+        this.dataSource.sort = this.sort;
+    });        
   }
 
+  // Aplica filtro a la lista de productos
+  applyFilter(filterValue: string): void {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  openDialog(action: string, obj: any): void {
+    obj.action = action;
+    //obj.type = action == 'Nuevo' ? 1 : 2;
+    //obj.uId = AuthService.getUser().id;
+    const dialogRef = this.dialog.open(ProductsDialogComponent, {
+        data: obj
+    });
+    dialogRef.afterClosed().subscribe(result => {
+        if (result.event === 'Nuevo Producto') {
+            // this.addRowData(result.data);
+        } else if (result.event === 'Actualizar') {
+            // this.updateRowData(result.data);
+        } else if (result.event === 'Eliminar') {
+            // this.deleteRowData(result.data);
+        }
+    }); 
+}
 
   ngAfterViewInit(): void {
     // this.insurerService.getInsurer().subscribe(response => {
@@ -169,29 +92,6 @@ export class ProductsComponent implements OnInit, AfterViewInit {
     // });
     // Mandar llamar el servicio para rellenar el arreglo de productos (productos a mostrar)
   }
-
-
-  applyFilter(value) {
-    this.dataSource.filter = value;
-  }
-
-  openDialog(data: string, obj: any): void {
-    obj.action = data;
-    obj.type = (data == "Nueva") ? 1 : 2;
-    //!!! obj.userId = 4 // Obtener verdadero user
-
-    this.dialog.open(ProductsDialogComponent, {
-      data: obj,
-      width: '40%'
-    }).afterClosed().subscribe((confirm: boolean) => {
-      if (confirm) {
-        this.ngAfterViewInit();
-      } else {
-        // this.toastr.error('Cancelado');
-      }
-    });
-  }
-
 
   confirmDialog(element) {
     // const dialogRef = this.dialog.open(ConfirmDialogComponent, {
