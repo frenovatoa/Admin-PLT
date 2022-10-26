4ccb05a4986609f5717c43a97fdd801c8a2a24f4
Claudia Hernandez <claudiafl2002@outlook.com>
2022-10-26 16:25:20 -0500

    Hice cambios para subir imagenes

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -1,13 +1,26 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import moment from 'moment';
+import { from, Observable, of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { User } from '../interfaces/user';
+import { AuthService } from '../auth/auth.service';
+import {
+  collection,
+  doc,
+  docData,
+  Firestore,
+  getDoc,
+  setDoc,
+  updateDoc,
+} from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor(private db: AngularFirestore) { }
+  constructor(private db: AngularFirestore,private firestore: Firestore, private authService: AuthService) { }
 
   // Función que permite obtener los documentos (registros) de la tabla de usuarios en la base de datos
   // Solo me trae los usuarios con estatus true, es decir, que si están activos
@@ -28,7 +41,19 @@ export class UserService {
   updateUser(uid: any, data: any) {
     this.db
     .doc(`tbl_users/${uid}`)
-    .update({...data})
+    .update({uid: data.uid,
+      userTypeId: data.userTypeId,
+      name: data.name,
+      paternalLastName: data.paternalLastName,
+      maternalLastName: data.maternalLastName,
+      email: data.email,
+      password: data.password,
+      status: data.status})
+  }
+
+  updateUserTest(user: User): Observable<void> {
+    const ref = doc(this.firestore, 'tbl_users', user.uid);
+    return from(updateDoc(ref, { ...user }));
   }
 
 
@@ -49,5 +74,19 @@ export class UserService {
     today.year() +
     Math.random().toString(36).substring(2, 15)
   );
+  }
+
+
+  get currentUserProfile$(): Observable<User | null> {
+    return this.authService.currentUser$.pipe(
+      switchMap((user) => {
+        if (!user?.uid) {
+          return of(null);
+        }
+
+        const ref = doc(this.firestore, 'users', user?.uid);
+        return docData(ref) as Observable<User>;
+      })
+    );
   }
 }
