68b868b936d679f59c591a7df98bc3e9dc6c5e61
Jose Bautista <bautistalarajosenatividad@gmail.com>
2022-11-06 20:08:24 -0600

    Cambios en el table de Sale

--- src/app/shared/interfaces/product.ts
+++ src/app/shared/interfaces/product.ts
@@ -1,7 +1,10 @@
+
 export interface Product {
     id: string;
     productTypeId: string;
     productTypeDescription?: string;
+    productRetail?: number;
+    productWholesale?: number;
     description: string;
     quantity: number;
     image: string;
